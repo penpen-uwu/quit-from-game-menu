6c7d41b73bf1b46cb9364c4458c8da22fd0a690c
penpen-uwu <71316821+penpen-uwu@users.noreply.github.com>
2025-11-03 11:32:35 +0900

    Support for MC versions 1.19.4 - 1.21.5, remove unnecessary Fabric API version thing from gradle
    ^^ I forgot to commit that
    
    also switch to mojang mapping

--- src/client/java/penpen/quitfromgamemenu/mixin/client/GameMenuScreenMixin.java
+++ src/client/java/penpen/quitfromgamemenu/mixin/client/GameMenuScreenMixin.java
@@ -1,30 +1,30 @@
 package penpen.quitfromgamemenu.mixin.client;
 
 import com.llamalad7.mixinextras.sugar.Local;
-import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.gui.screen.GameMenuScreen;
-import net.minecraft.client.gui.screen.Screen;
-import net.minecraft.client.gui.widget.ButtonWidget;
-import net.minecraft.client.gui.widget.GridWidget;
-import net.minecraft.text.Text;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.components.Button;
+import net.minecraft.client.gui.layouts.GridLayout;
+import net.minecraft.client.gui.screens.PauseScreen;
+import net.minecraft.client.gui.screens.Screen;
+import net.minecraft.network.chat.Component;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(GameMenuScreen.class)
+@Mixin(PauseScreen.class)
 public abstract class GameMenuScreenMixin extends Screen {
 
-    protected GameMenuScreenMixin(Text title) {
-		super(title);
-	}
+    protected GameMenuScreenMixin(Component title) {
+        super(title);
+    }
 
-	@Inject(method = "initWidgets", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/widget/GridWidget;refreshPositions()V"))
-	private void addQuitGameButton(CallbackInfo ci, @Local GridWidget.Adder adder) {
-        adder.add(ButtonWidget.builder(Text.translatable("menu.quit"), (button) -> {
+    @Inject(method = "createPauseMenu", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/layouts/GridLayout;arrangeElements()V"))
+    private void addQuitGameButton(CallbackInfo ci, @Local GridLayout.RowHelper adder) {
+        adder.addChild(Button.builder(Component.translatable("menu.quit"), (button) -> {
             button.active = false;
-            MinecraftClient.getInstance().scheduleStop();
+            Minecraft.getInstance().stop();
         }).width(204).build(), 2);
-	}
+    }
 }
 
